d026421ba8b6da9e2f6f882ffd6afcc8bc6c2811
nhuquynh10 <toannguyen.programmer@gmail.com>
2023-02-20 10:30:22 +0700

    commit

--- WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -38,9 +38,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(119, 64);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.Size = new System.Drawing.Size(113, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "label1 version 1";
             // 
             // label2
             // 
